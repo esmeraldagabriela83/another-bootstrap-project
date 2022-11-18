3bf41c7108d5ff84aa5f074d319b8c252d421963
Acornicesei Mihaela Gabriela <esmeraldagabriela83@yahoo.com>
2022-11-18 12:34:33 +0200

    second second commit

--- mybootstrapapp/src/App.js
+++ mybootstrapapp/src/App.js
@@ -1,10 +1,18 @@
-
+import React, {useState,useEffect} from 'react';
 //import './App.css';
+
+//pedro course
+//https://www.youtube.com/watch?v=f55qeKGgB_M&t=616s
 
 function App() {
   return (
     <div className="App">
     new app bootstrap
+
+    new app bootstrap
+
+    new app bootstrap
+
     </div>
   );
 }
