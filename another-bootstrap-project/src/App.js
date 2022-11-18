3297dae95829cca12c79fd7096544d34ceca0cf4
Acornicesei Mihaela Gabriela <esmeraldagabriela83@yahoo.com>
2022-11-18 12:03:28 +0200

    second commit

--- another-bootstrap-project/src/App.js
+++ another-bootstrap-project/src/App.js
@@ -1,10 +1,19 @@
 import React, {useState,useEffect} from 'react';
 //import './App.css';
 
+//pedro course
+//https://www.youtube.com/watch?v=f55qeKGgB_M&t=616s
+
 function App() {
 
 const name="Mihaela";
 const secondName=<h1>hello Gabriela</h1>;
+const myObj={
+  catName:"micu",
+  catAge:5,
+  friends:"firutu"
+};
+
 
   return (
     <div className="App">
@@ -12,6 +21,7 @@ const secondName=<h1>hello Gabriela</h1>;
   <h1> hello {name} </h1>
   <h1> hello {name} </h1>
   {secondName}
+  <h3>my cat is named : {myObj.catName} , it has {myObj.catAge} and one friend called {myObj.friends}</h3>
     </div>
   );
 }
